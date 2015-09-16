3ae14e1e5451dab55c8d14139e66e07b9d339a48
René Mayorga <rmayorga@debian.org.sv>
2015-09-15 19:10:42 -0600

    Living with the wrong output

--- bot.pl
+++ bot.pl
@@ -1630,7 +1630,7 @@ sub fortune {
 
 sub clima {
 	my $city = shift;
-	my $clima = `ansiweather -l \"$city\" -a false -s false`;
+	my $clima = `ansiweather -a false -s false -l city`;
 	return $clima;
 }
 
