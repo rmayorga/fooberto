05447314ad1508d3f4626355b9d0ab5c4aa2ea48
René Mayorga <rmayorga@debian.org.sv>
2015-09-15 19:12:27 -0600

    Silly errors

--- bot.pl
+++ bot.pl
@@ -1630,7 +1630,7 @@ sub fortune {
 
 sub clima {
 	my $city = shift;
-	my $clima = `ansiweather -a false -s false -l city`;
+	my $clima = `ansiweather -a false -s false -l $city`;
 	return $clima;
 }
 
